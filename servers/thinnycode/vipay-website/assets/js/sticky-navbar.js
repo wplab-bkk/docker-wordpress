4b2042d6e41452984e82323c9e93ccd39a42561f
sutin1234 <tony.sutin1234@gmail.com>
2019-11-12 19:56:37 +0000

    add backup assets all schedue

---
+++ servers/thinnycode/vipay-website/assets/js/sticky-navbar.js
@@ -0,0 +1,425 @@
+
+// Sticky Header
+window.theme = {};
+
+(function(theme, $) {
+
+	theme = theme || {};
+
+	var initialized = false;
+
+	$.extend(theme, {
+
+		StickyHeader: {
+
+			defaults: {
+				wrapper: $('#header'),
+				headerBody: $('#header .header-body'),
+				stickyEnabled: true,
+				stickyEnableOnBoxed: true,
+				stickyEnableOnMobile: true,
+				stickyStartAt: 0,
+				stickyStartAtElement: false,
+				stickySetTop: 0,
+				stickyEffect: '',
+				stickyHeaderContainerHeight: false,
+				stickyChangeLogo: false,
+				stickyChangeLogoWrapper: true
+			},
+
+			initialize: function($wrapper, opts) {
+				if (initialized) {
+					return this;
+				}
+
+				initialized = true;
+				this.$wrapper = ($wrapper || this.defaults.wrapper);
+
+				this
+					.setOptions(opts)
+					.build()
+					.events();
+
+				return this;
+			},
+
+			setOptions: function(opts) {
+				this.options = $.extend(true, {}, this.defaults, opts, theme.fn.getOptions(this.$wrapper.data('plugin-options')));
+
+				return this;
+			},
+
+			build: function() {
+				if (!this.options.stickyEnableOnBoxed && $('html').hasClass('boxed') || $('html').hasClass('side-header-hamburguer-sidebar') || !this.options.stickyEnabled) {
+					return this;
+				}
+
+				var self = this,
+					$html = $('html'),
+					$window = $(window),
+					sideHeader = $html.hasClass('side-header'),
+					initialHeaderTopHeight = self.options.wrapper.find('.header-top').outerHeight(),
+					initialHeaderContainerHeight = self.options.wrapper.find('.header-container').outerHeight(),
+					minHeight;
+
+				// HTML Classes
+				$html.addClass('sticky-header-enabled');
+
+				if (parseInt(self.options.stickySetTop) < 0) {
+					$html.addClass('sticky-header-negative');
+				}
+
+				// Set Start At
+				if(self.options.stickyStartAtElement) {
+
+					var $stickyStartAtElement = $(self.options.stickyStartAtElement);
+
+					$(window).on('scroll resize', function() {
+						self.options.stickyStartAt = $stickyStartAtElement.offset().top;
+					});
+
+					$(window).trigger('resize');
+				}
+
+				// Define Min Height value
+				if( self.options.wrapper.find('.header-top').get(0) ) {
+					minHeight = ( initialHeaderTopHeight + initialHeaderContainerHeight );
+				} else {
+					minHeight = initialHeaderContainerHeight;
+				}
+
+				// Set Wrapper Min-Height
+				if( !sideHeader ) {
+					if( !$('.header-logo-sticky-change').get(0) ) {
+						self.options.wrapper.css('height', self.options.headerBody.outerHeight());
+					} else {
+						$window.on('stickyChangeLogo.loaded', function(){
+							self.options.wrapper.css('height', self.options.headerBody.outerHeight());
+						});
+					}
+
+					if( self.options.stickyEffect == 'shrink' ) {
+						
+						// Prevent wrong visualization of header when reload on middle of page
+						$(document).ready(function(){
+							if( $window.scrollTop() >= self.options.stickyStartAt ) {
+								self.options.wrapper.find('.header-container').on('transitionend webkitTransitionEnd oTransitionEnd', function(){
+									self.options.headerBody.css('position', 'fixed');
+								});
+							} else {
+								self.options.headerBody.css('position', 'fixed');
+							}
+						});
+
+						self.options.wrapper.find('.header-container').css('height', initialHeaderContainerHeight);
+						self.options.wrapper.find('.header-top').css('height', initialHeaderTopHeight);
+					}
+				}
+
+				// Sticky Header Container Height
+				if( self.options.stickyHeaderContainerHeight ) {
+					self.options.wrapper.find('.header-container').css('height', self.options.wrapper.find('.header-container').outerHeight());
+				}
+
+				// Boxed
+				if($html.hasClass('boxed') && self.options.stickyEffect == 'shrink') {
+					if( (parseInt(self.options.stickyStartAt) == 0) && $window.width() > 991) {
+						self.options.stickyStartAt = 30;
+					}
+
+					// Set Header Body Position Absolute
+					self.options.headerBody.css('position','absolute');
+
+					// Set position absolute because top margin from boxed layout
+					$window.on('scroll', function(){
+						if( $window.scrollTop() > $('.body').offset().top ) {
+							self.options.headerBody.css({
+								'position' : 'fixed',
+								'top' : 0
+							});								
+						} else {
+							self.options.headerBody.css({
+								'position' : 'absolute',
+								'top' : 0
+							});
+						}
+					});
+				}
+
+				// Check Sticky Header / Flags prevent multiple runs at same time
+				var activate_flag   = true,
+					deactivate_flag = false;
+
+				self.checkStickyHeader = function() {
+					if( $window.width() > 991 && $html.hasClass('side-header') ) {
+						$html.removeClass('sticky-header-active');
+						activate_flag = true;
+						return;
+					}
+
+					if ($window.scrollTop() >= parseInt(self.options.stickyStartAt)) {
+						if( activate_flag ) {
+							self.activateStickyHeader();
+							activate_flag = false;
+							deactivate_flag = true;
+						}
+					} else {
+						if( deactivate_flag ) {
+							self.deactivateStickyHeader();
+							deactivate_flag = false;
+							activate_flag = true;
+						}
+					}
+				};
+				
+				// Activate Sticky Header
+				self.activateStickyHeader = function() {
+
+					if ($window.width() < 992) {
+						if (!self.options.stickyEnableOnMobile) {
+							self.deactivateStickyHeader();
+							return;
+						}
+					} else {
+						if (sideHeader) {
+							self.deactivateStickyHeader();
+							return;
+						}
+					}
+
+					$html.addClass('sticky-header-active');
+
+					// Sticky Effect - Reveal
+					if( self.options.stickyEffect == 'reveal' ) {
+
+						self.options.headerBody.css('top','-' + self.options.stickyStartAt + 'px');
+
+						self.options.headerBody.animate({
+							top: self.options.stickySetTop
+						}, 400, function() {});
+
+					}
+
+					// Sticky Effect - Shrink
+					if( self.options.stickyEffect == 'shrink' ) {
+
+						// If Header Top
+						if( self.options.wrapper.find('.header-top').get(0) ) {
+							self.options.wrapper.find('.header-top').css({
+								height: 0,
+								'min-height': 0,
+								overflow: 'hidden'
+							});
+						}
+
+						// Header Container
+						if( self.options.stickyHeaderContainerHeight ) {
+							self.options.wrapper.find('.header-container').css({
+								height: self.options.stickyHeaderContainerHeight,
+								'min-height': 0
+							});
+						} else {
+							self.options.wrapper.find('.header-container').css({
+								height: (initialHeaderContainerHeight / 3) * 2, // two third of container height
+								'min-height': 0
+							});
+
+							var y = initialHeaderContainerHeight - ((initialHeaderContainerHeight / 3) * 2);
+							$('.main').css({
+								transform: 'translate3d(0, -'+ y +'px, 0)',
+								transition: 'ease transform 300ms'
+							});
+
+							if($html.hasClass('boxed')) {
+								self.options.headerBody.css('position','fixed');
+							}
+						}
+
+					}
+
+					self.options.headerBody.css('top', self.options.stickySetTop);
+
+					if (self.options.stickyChangeLogo) {
+						self.changeLogo(true);
+					}
+
+					// Set Elements Style
+					$('[data-sticky-header-style]').each(function() {
+						var $el = $(this),
+							css = theme.fn.getOptions($el.data('sticky-header-style-active')),
+							opts = theme.fn.getOptions($el.data('sticky-header-style'));
+
+						if( $window.width() > opts.minResolution ) {
+							$el.css(css);
+						}
+					});
+
+					$.event.trigger({
+						type: 'stickyHeader.activate'
+					});
+				};
+
+				// Deactivate Sticky Header
+				self.deactivateStickyHeader = function() {
+
+					$html.removeClass('sticky-header-active');
+
+					// Sticky Effect - Shrink
+					if( self.options.stickyEffect == 'shrink' ) {
+
+						// Boxed Layout
+						if( $html.hasClass('boxed') ) {
+
+							// Set Header Body Position Absolute
+							self.options.headerBody.css('position','absolute');
+
+							if( $window.scrollTop() > $('.body').offset().top ) {
+								// Set Header Body Position Fixed
+								self.options.headerBody.css('position','fixed');								
+							}
+
+						} else {
+							// Set Header Body Position Fixed
+							self.options.headerBody.css('position','fixed');
+						}
+
+						// If Header Top
+						if( self.options.wrapper.find('.header-top').get(0) ) {
+							self.options.wrapper.find('.header-top').css({
+								height: initialHeaderTopHeight,
+								overflow: 'visible'
+							});
+						}
+
+						// Header Container
+						self.options.wrapper.find('.header-container').css({
+							height: initialHeaderContainerHeight
+						});
+
+					}
+
+					self.options.headerBody.css('top', 0);
+
+					if (self.options.stickyChangeLogo) {
+						self.changeLogo(false);
+					}
+
+					// Set Elements Style
+					$('[data-sticky-header-style]').each(function() {
+						var $el = $(this),
+							css = theme.fn.getOptions($el.data('sticky-header-style-deactive')),
+							opts = theme.fn.getOptions($el.data('sticky-header-style'));
+
+						if( $window.width() > opts.minResolution ) {
+							$el.css(css);
+						}
+					});
+
+					$.event.trigger({
+						type: 'stickyHeader.deactivate'
+					});
+				};
+
+				// Always Sticky
+				if (parseInt(self.options.stickyStartAt) <= 0) {
+					self.activateStickyHeader();
+				}
+
+				// Notice Top Bar
+				if ($('.notice-top-bar').get(0)) {
+					self.options.stickyStartAt = $('.notice-top-bar').outerHeight();
+				}
+
+				// Set Logo
+				if (self.options.stickyChangeLogo) {
+
+					var $logoWrapper = self.options.wrapper.find('.header-logo'),
+						$logo = $logoWrapper.find('img'),
+						logoWidth = $logo.attr('width'),
+						logoHeight = $logo.attr('height'),
+						logoSmallTop = parseInt($logo.attr('data-sticky-top') ? $logo.attr('data-sticky-top') : 0),
+						logoSmallWidth = parseInt($logo.attr('data-sticky-width') ? $logo.attr('data-sticky-width') : 'auto'),
+						logoSmallHeight = parseInt($logo.attr('data-sticky-height') ? $logo.attr('data-sticky-height') : 'auto');
+
+					if (self.options.stickyChangeLogoWrapper) {
+						$logoWrapper.css({
+							'width': $logo.outerWidth(true),
+							'height': $logo.outerHeight(true)
+						});
+					}
+
+					self.changeLogo = function(activate) {
+						if(activate) {
+							
+							$logo.css({
+								'top': logoSmallTop,
+								'width': logoSmallWidth,
+								'height': logoSmallHeight
+							});
+
+						} else {
+							
+							$logo.css({
+								'top': 0,
+								'width': logoWidth,
+								'height': logoHeight
+							});
+
+						}
+					}
+
+					$.event.trigger({
+						type: 'stickyChangeLogo.loaded'
+					});
+
+				}
+
+				// Side Header
+				var headerBodyHeight,
+					flag = false;
+
+				self.checkSideHeader = function() {
+					if($window.width() < 992 && flag == false) {
+						headerBodyHeight = self.options.headerBody.height();
+						flag = true;
+					}
+
+					if(self.options.stickyStartAt == 0 && sideHeader) {
+						self.options.wrapper.css('min-height', 0);
+					}
+
+					if(self.options.stickyStartAt > 0 && sideHeader && $window.width() < 992) {
+						self.options.wrapper.css('min-height', headerBodyHeight);
+					}
+				}
+
+				return this;
+			},
+
+			events: function() {
+				var self = this;
+
+				if (!this.options.stickyEnableOnBoxed && $('body').hasClass('boxed') || $('html').hasClass('side-header-hamburguer-sidebar') || !this.options.stickyEnabled) {
+					return this;
+				}
+
+				if (!self.options.alwaysStickyEnabled) {
+					$(window).on('scroll resize', function() {
+						self.checkStickyHeader();
+					});
+				} else {
+					self.activateStickyHeader();
+				}
+
+				$(window).on('load resize', function(){
+					self.checkSideHeader();
+				});
+
+				return this;
+			}
+
+		}
+
+	});
+
+}).apply(this, [window.theme, jQuery]);
